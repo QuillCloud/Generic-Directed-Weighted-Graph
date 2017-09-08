abcfaa13495d4261d7f8b534075998eb70d304b9
awszwc <xlb25@126.com>
2017-09-09 06:11:57 +1000

    constructor copy/move constrctor/assignment addNode addEdge

--- Graph.h
+++ Graph.h
@@ -17,6 +17,8 @@ namespace gdwg {
         Graph(const Graph&);
         Graph(Graph&&);
         ~Graph();
+        Graph& operator=(const Graph&);
+        Graph& operator=(Graph&&);
         bool addNode(const N&);
         bool addEdge(const N&, const N&, const E&);
         bool replace(const N&, const N&);
@@ -61,13 +63,35 @@ namespace gdwg {
         std::cout << "destructor" << std::endl;
     }
 
+    // copy assignment
+    template <typename N, typename E>
+    Graph<N, E>& Graph<N, E>::operator=(const Graph& graph) {
+        *node_list = *graph.node_list;
+        *edge_list = *graph.edge_list;
+        std::cout << "copy assignment" << std::endl;
+        return *this;
+    }
+
+    // move assignment
+    template <typename N, typename E>
+    Graph<N, E>& Graph<N, E>::operator=(Graph&& graph) {
+        *node_list = std::move(*graph.node_list);
+        *edge_list = std::move(*graph.edge_list);
+        graph.node_list = nullptr;
+        graph.edge_list = nullptr;
+        std::cout << "move assignment" << std::endl;
+        return *this;
+    }
+
     // addNode
     template <typename N, typename E>
     bool Graph<N, E>::addNode(const N& val) {
+        // return false if node already in graph
         if (std::find(node_list->begin(), node_list->end(), val) != node_list->end()) {
             std::cout << "add node \"" << val << "\" fail" << std::endl;
             return false;
         } else {
+            // add node to graph
             node_list->push_back(val);
             std::cout << "add node \"" << val << "\" success" << std::endl;
             return true;
@@ -77,16 +101,20 @@ namespace gdwg {
     // addEdge
     template <typename N, typename E>
     bool Graph<N, E>::addEdge(const N& src, const N& dst, const E& w) {
+        // throw runtime_error if src or dst is not in the graph
         if (std::find(node_list->begin(), node_list->end(), src) == node_list->end()) {
             throw std::runtime_error("src is not in the graph");
         } else if (std::find(node_list->begin(), node_list->end(), dst) == node_list->end()) {
             throw std::runtime_error("dst is not in the graph");
         } else {
+            // create the the edge that will be added
             auto tp = std::make_tuple(src, dst, w);
+            // return false if edge already in graph
             if (std::find(edge_list->begin(), edge_list->end(), tp) != edge_list->end()) {
                 std::cout << "add edge (" << src << ", " << dst << ", " << w << ") fail" << std::endl;
                 return false;
             } else {
+                // add edge to graph
                 edge_list->push_back(tp);
                 std::cout << "add edge (" << src << ", " << dst << ", " << w << ") success" << std::endl;
                 return true;
@@ -97,22 +125,30 @@ namespace gdwg {
     // replace
     template <typename N, typename E>
     bool Graph<N, E>::replace(const N& oldData, const N& newData) {
+        // throw runtime_error if graph do not contain old data
         if (std::find(node_list->begin(), node_list->end(), oldData) == node_list->end()) {
             throw std::runtime_error("no node that contains value oldData can be found");
         } else {
-            if (std::find(node_list->begin(), node_list->end(), oldData) == node_list->end()) {
+            // return false if new data already in graph
+            if (std::find(node_list->begin(), node_list->end(), newData) != node_list->end()) {
+                std::cout << "replace node \"" << oldData << "\" with \"" << newData << "\" fail" << std::endl;
                 return false;
             } else {
+                // replace the old data with new data in node list
                 std::replace(node_list->begin(), node_list->end(), oldData, newData);
+                // replace the old data with new data in edge list
                 for (auto& edge : *edge_list) {
-                    std::cout << std::get<0>(edge) << std::endl;
+                    if (std::get<0>(edge) == oldData) {
+                        std::get<0>(edge) = newData;
+                    } else if (std::get<1>(edge) == oldData) {
+                        std::get<1>(edge) = newData;
+                    }
                 }
+                std::cout << "replace node \"" << oldData << "\" with \"" << newData << "\" success" << std::endl;
+                return true;
             }
         }
     }
-
-
-
 };
 
 #endif //GENERIC_DIRECTED_WEIGHTED_GRAPH_GRAPH_H
