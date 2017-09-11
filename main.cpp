8fc85f4b88c8a00f592357c108e12ec38103c7dc
awszwc <xlb25@126.com>
2017-09-11 22:21:00 +1000

    a

--- main.cpp
+++ main.cpp
@@ -6,10 +6,5 @@ int main() {
 
     // make a graph
     gdwg::Graph<unsigned int, int> g;
-    gdwg::Graph<unsigned int, int> b(g);
-    if (b.addNode(1))
-        std::cout << "add 1 to b" << std::endl;
-    if (g.addNode(1))
-        std::cout << "add 1 to g" << std::endl;
 
 }
